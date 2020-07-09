2db67ab0ba31979ee4c569101afe705a393855a6
Eddie Lopez <iameddielopez@gmail.com>
2020-07-09 22:59:43 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,10 @@
 # Write your code here
 count = 10
 celebration_number = 0
-while count > 0  do
+while count > 0 do
+  break if count == celebration_number 
+  puts "Happy New Year"
+  end
   puts "#{count}"
-  count -= 1 
-  
-
+  count -= 1
 end 
